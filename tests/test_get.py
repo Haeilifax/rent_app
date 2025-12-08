e7a5ab14c7b443835d8241b7a030cf75e0347ab5
David A <davidbarcuri@gmail.com>
2025-12-08 17:09:38 -0500

    Add test for rent remaining showing wrong

--- tests/test_get.py
+++ tests/test_get.py
@@ -2,20 +2,19 @@ import pytest
 import sqlite3
 from pathlib import Path
 import os
-
+import importlib.resources
+import datetime
 
 import rent_app
 
+db_path = Path(os.environ["ISLOCAL"])
 
 @pytest.fixture(autouse=True)
 def setup_db(base_db_bytes: bytes):
-    db_path = Path(os.environ["ISLOCAL"])
     db_path.write_bytes(base_db_bytes)
 
 
 def test_stylesheet():
-    import importlib.resources
-
     true_css = (
         importlib.resources.files(rent_app)
         .joinpath("templates", "stylesheet.css")
@@ -38,3 +37,17 @@ def test_homepage():
     )
     assert response["statusCode"] == 200
     assert response["headers"]["Content-Type"] == "text/html"
+
+def test_no_collected_rents_shows_full_rent_remaining():
+    get_rents_sql = (
+        importlib.resources.files(rent_app)
+        .joinpath("get_rents.sql")
+        .read_text()
+    )
+    check_no_collected_rents = "SELECT COUNT(1) FROM CollectedRent WHERE collected_on > '2100-01-01'"
+    db = sqlite3.connect(db_path)
+    num_collected_rents = db.execute(check_no_collected_rents).fetchall()[0][0]
+    assert num_collected_rents == 0
+
+    rents = db.execute(get_rents_sql, {"month": datetime.datetime.now().isoformat()}).fetchall()
+    assert rents[0][3] == rents[0][2]
